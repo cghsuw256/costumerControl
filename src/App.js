b17f6109eddac189f4b4c786a7f37602b9c1a618
cgh <c66182527@gmail.com>
2022-07-17 00:11:09 +0900

    5강 컴포넌트 구조화하기

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Customer from './components/Customer';
 
-function App() {
-  return (
-    <div className='gray-background'>
-      <img src={logo} lat='logo'/>
-      <h2>Let's develop management system!</h2>
-    </div>
-  );
+const customer = {
+  'id' : 1,
+  'image' : 'https://placeimg.com/64/64/any',
+  'name' : '홍길동',
+  'birthday' : '961222',
+  'gender' : '남자',
+  'job' : '대학생'
+}
+
+const App = () => {
+    return (
+      <div>
+        <Customer 
+          id = {customer.id}
+          image = {customer.image}
+          name={customer.name}
+          birthday={customer.birthday}
+          gender={customer.gender}
+          job={customer.job}
+        />
+      </div>
+   );
 }
 
 export default App;
